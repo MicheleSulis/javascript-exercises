9dd9205317d7ff4aafc96c00b855aa1b9de6a354
Michele Sulis <sulismichele00@gmail.com>
2025-11-12 02:13:18 +0100

    Accomplish 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (start) {
+    let trats = "";
+    for (let i = start.length - 1; i >= 0; i--)
+        trats += start[i];
+    return trats;
 };
 
 // Do not edit below this line
