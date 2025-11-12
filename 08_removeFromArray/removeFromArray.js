8c3b029ccf88dc122ba27d5def59d4450f513490
Michele Sulis <sulismichele00@gmail.com>
2025-11-12 04:04:20 +0100

    Accomplish 08 and start 09

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+    for (let arg of args)
+        for (let i = 0; i < array.length; i++)
+            if (arg === array[i]) {
+                array.splice(i, 1);
+                i--;
+            }
+    return array;
 };
+
+removeFromArray([1, 2, 2, 3], 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+    return [...Array(end + 1)].reduce((total, index) => {
+        return total + index;
+    }, start);
 };
+
+a = sumAll(10, 5);
 
 // Do not edit below this line
 module.exports = sumAll;
